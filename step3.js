06f61b9af0dfe64776b28b295eef44bd50ad5c7b
Juan Cruz <jc23729@gmail.com>
2021-02-13 07:37:04 -0500

    update

--- step3.js
+++ step3.js
@@ -1,6 +1,35 @@
-const fs = require('fs');
-const process = require('process');
-const axios = require('axios');
+// Step 3
+// Copy over your step2.js code to step3.js.
+
+// Add a feature where, on the command line, you can optionally provide an argument to output to a file instead of printing to the console. The argument should look like this: --out output-filename.txt readfile-or-url.
+
+// Current features should still work the same:
+
+// $ node step3.js one.txt
+// This is file one.
+
+// $ node step3.js http://google.com
+// <!doctype html><html ...
+// However, if --out follows your script name, it should take the next argument and use that as the path to write to.
+
+// For example:
+
+// $ node step3.js --out new.txt one.txt
+// $ # no output, but new.txt contains contents of one.txt
+
+// $ node step3.js --out new.txt  http://google.com
+// $ # no output, but new.txt contains google's HTML
+// Make sure you handle errors trying to write to the file:
+
+// $ node step3.js --out /no/dir/new.txt one.txt
+// Couldn't write /no/dir/new.txt:
+//   Error: ENOENT: no such file or directory, open '/no/dir/new.txt'
+
+
+
+// const fs = require('fs');
+// const process = require('process');
+// const axios = require('axios');
 
 /** handle output: write to file if out given, else print */
 
